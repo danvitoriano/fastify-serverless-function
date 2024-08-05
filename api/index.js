0a921cbde25208dd92c681a0a94d98996602f1f9
Dan Vitoriano <vitoriano08@gmail.com>
2024-08-04 23:09:38 -0300

    add cors header

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import Fastify from 'fastify'
 
+
 const app = Fastify({
   logger: true,
 })
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.orgulhotech.com.br'); 
+  // Or res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get('/', async (req, reply) => {
   return reply.status(200).type('text/html').send()
