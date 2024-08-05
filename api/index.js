b484a9e1afb81e8459b18e84922f9985c4e943a3
Dan Vitoriano <vitoriano08@gmail.com>
2024-08-04 23:52:00 -0300

    cors

--- api/index.js
+++ api/index.js
@@ -5,21 +5,13 @@ const app = Fastify({
   logger: true,
 })
 
-app.route({
-  method: 'POST',
-  url: '/api/email/subscribe',
-  handler: (request, reply) => {
-    reply.header('Access-Control-Allow-Origin', 'https://sites-orgulho-tech--staging.decocdn.com');  // Replace with allowed origin(s)
-    reply.header('Access-Control-Allow-Methods', 'POST'); // Specify allowed methods
-
-    
-    return { email: request.body.email }
-    
-  }
-});
 
 app.get('/', async (req, reply) => {
   return reply.status(200).type('text/html').send()
+})
+
+app.post('/api/email/subscribe', async (req, reply) => {
+  return { email: req.body.email }
 })
 
 export default async function handler(req, reply) {
