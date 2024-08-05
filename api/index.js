c0e90d5703627557ea2223958c3740e58e48fc8d
Dan Vitoriano <vitoriano08@gmail.com>
2024-08-04 23:11:15 -0300

    cors

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ const app = Fastify({
 })
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.orgulhotech.com.br'); 
-  // Or res.setHeader('Access-Control-Allow-Origin', '*'); 
+  // res.setHeader('Access-Control-Allow-Origin', 'https://www.orgulhotech.com.br'); 
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
